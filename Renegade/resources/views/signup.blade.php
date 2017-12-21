109e6454ad35e7418b1793f2fd5367cf56bfd704
albin <padinju@gmail.com>
2017-12-21 14:40:33 +0530

    welcom

---
+++ Renegade/resources/views/signup.blade.php
@@ -0,0 +1,37 @@
+@extends('master')
+
+@section('title')
+Sign Up
+@endsection
+
+@section('content')
+    <h3>Sign Up</h3>
+        <div class="col-md-6">
+                <form action="#" method="post">
+                  <div class='form-group'>
+                    <label>Email</label>
+                    <input class='form-control' type='text' name='email' id='email'>
+                  </div>
+                  <div class='form-group'>
+                    <label>First Name</label>
+                    <input class='form-control' type='text' name='first_name' id='first_name'>
+                  </div>
+                  <div class='form-group'>
+                    <label>Last Name</label>
+                    <input class='form-control' type='text' name='last_name' id='last_name'>
+                  </div>
+                  <div class='form-group'>
+                    <label>Password</label>
+                    <input class='form-control' type='password' name='password' id='password'>
+                  </div>
+                  <div class='form-group'>
+                    <label>Channel</label>
+                    <input class='form-control' type='text' name='channel' id='channel'>
+                  </div>
+                    <button type='submit' class='btn btn-primary'>Submit</button>
+                </form>
+
+            </div>
+        </div>
+
+@endsection
--- Renegade/resources/views/welcome.blade.php
+++ Renegade/resources/views/welcome.blade.php
@@ -1,37 +1,13 @@
 @extends('master')
 
 @section('title')
-Sign Up
-@endsection
-
-@section('content')
-    <h3>Sign Up</h3>
-        <div class="col-md-6">
-                <form action="#" method="post">
-                  <div class='form-group'>
-                    <label>Email</label>
-                    <input class='form-control' type='text' name='email' id='email'>
-                  </div>
-                  <div class='form-group'> 
-                    <label>First Name</label>
-                    <input class='form-control' type='text' name='first_name' id='first_name'>
-                  </div>
-                  <div class='form-group'> 
-                    <label>Last Name</label>
-                    <input class='form-control' type='text' name='last_name' id='last_name'>
-                  </div>
-                  <div class='form-group'> 
-                    <label>Password</label>
-                    <input class='form-control' type='password' name='password' id='password'>
-                  </div>
-                  <div class='form-group'> 
-                    <label>Channel</label>
-                    <input class='form-control' type='text' name='channel' id='channel'>
-                  </div>
-                    <button type='submit' class='btn btn-primary'>Submit</button>
-                </form>
-                        
-            </div>    
-        </div>
+RENEGADE
 
 @endsection
+
+<header>
+<h1>RENEGADE</h1>
+<a href="signup">  <button type="button" class="btn btn-primary"  >signup</button><a>
+<a href="signin">  <button type="button" class="btn btn-primary"  >signin</button><a>
+
+<header>

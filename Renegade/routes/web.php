aa6ef1701b4280fb6873d9c22856ead702324799
albin <padinju@gmail.com>
2017-12-22 12:30:45 +0530

    route protection

--- Renegade/routes/web.php
+++ Renegade/routes/web.php
@@ -14,7 +14,7 @@
 
     Route::get('/', function () {
         return view('welcome');
-    });
+    })->name('login');
 
     Route::get('signup', function () {
         return view('signup');
@@ -35,5 +35,6 @@
 
     Route::get('dashboard',[
         'uses' => 'UserController@getDashboard',
-        'as' =>'dashboard'
-    ]);
+        'as' =>'dashboard',
+
+    ])->middleware('auth');

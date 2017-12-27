e9ec5dd59d788ddcb30f9eeefbda8e906f0c416f
Divya Moncy <divyaamoncy@gmail.com>
2017-12-27 16:08:04 +0530

    signin

--- Renegade/resources/views/signin.blade.php
+++ Renegade/resources/views/signin.blade.php
@@ -5,8 +5,12 @@ Sign In
 @endsection
 
 @section('content')
-    <h3>Sign In</h3>
-
+<link rel='stylesheet' href={{URL::to('src/css/signin.css')}}>
+<div class="toptext">
+    <h3 class="grey">Build your world.</h3>
+    <h3 class="grey">Yourself.</h3>
+    <h3 class="blacktext">Renegade</h3>
+</div>
     @if(count($errors) > 0)
         <div class="row">
           <div class="col-md-6">
@@ -18,20 +22,21 @@ Sign In
           </div>
         </div>
     @endif
+    <div class="row fields">
 
-        <div class="col-md-6">
+        <div class="col-md-6 col-xs-12 block">
                 <form action="{{route('postsignin')}}" method="post">
                   <div class='form-group {{$errors -> has('email') ? 'has-error' : '' }}'>
-                    <label>Email</label>
-                    <input class='form-control' type='text' name='email' id='email' value="{{ Request::old('email')}}">
+                    
+                    <input class='form-control' type='text' name='email' id='email' value="{{ Request::old('email')}}" placeholder="E-mail">
                   </div>
 
                   <div class='form-group {{$errors -> has('password') ? 'has-error' : '' }}'>
-                    <label>Password</label>
-                    <input class='form-control' type='password' name='password' id='password'>
+                    
+                    <input class='form-control' type='password' name='password' id='password' placeholder="Password">
                   </div>
 
-                    <button type='submit' class='btn btn-primary'>Submit</button>
+                    <button type='submit' class='bton'>Log In</button>
                     <input type='hidden' name='_token' value='{{Session::token()}}'>
                 </form>
 
